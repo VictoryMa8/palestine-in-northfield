ab0bad6f2a74e9b43bc705374a03492554cdb9b9
Victory Ma <victoryma23@gmail.com>
2025-07-28 08:40:57 -0500

    Added tailwind, daisyui, and favicon

--- backend/settings.py
+++ backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'frontend'
 ]
 
 MIDDLEWARE = [
@@ -108,17 +109,21 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
 USE_TZ = True
 
+STATIC_URL = 'frontend/static/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
