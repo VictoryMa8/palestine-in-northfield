7181cee9589390a496dfa8ce0604ffb09f2bd8c5
Victor <160278295+VictoryMa8@users.noreply.github.com>
2025-07-30 00:29:15 -0500

    Added nav, footer, and empty pages

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('resources/', views.resources, name='resources'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def resources(request):
+    return render(request, 'resources.html')
