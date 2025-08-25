45115e32b9aa0d3cb34a3f776ff5c533336e5008
Victory Ma <victoryma23@gmail.com>
2025-08-25 08:43:32 -0500

    Changed contact email

--- backend/settings.py
+++ backend/settings.py
@@ -124,9 +124,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'palestineinnorthfield@gmail.com'
+EMAIL_HOST_USER = 'victoryma23@gmail.com'
 EMAIL_HOST_PASSWORD = os.getenv('SMTP_PASS')
-DEFAULT_FROM_EMAIL = 'palestineinnorthfield@gmail.com'
+DEFAULT_FROM_EMAIL = 'victoryma23@gmail.com'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
